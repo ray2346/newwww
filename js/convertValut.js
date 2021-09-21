8a00abe34cdb6b4c688e14a7b62b95bf4145ec45
Ivan <tooshka4@yandex.ru>
2021-09-21 13:31:28 +0300

    ConvertValutrrrcf

--- js/convertValut.js
+++ js/convertValut.js
@@ -1,11 +1,25 @@
+'use strict';
 
 
-'use strict'
+
     const inputRub = document.querySelector('#rub'),
           inputUsd = document.querySelector('#usd');
 
-inputRub.addEventListener('input', ()=>{
+inputRub.addEventListener('input', ()=> {
     const request = new XMLHttpRequest();
 
-    request.open('get', method, url, async, login, pass);
+    request.open('get', 'js/current.json');
+    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    request.send();
+    request.addEventListener('readystatechange', () => {
+        if (request.readyState === 4 && request.status === 200) {
+            console.log(request.response)
+            const data = JSON.parse(request.response);
+            inputUsd.value= (+inputRub.value/data.current.usd).toFixed(2);
+        }
+        else {
+            inputUsd.value='Что-то пошло не так '
+        }
+
+    });
 });
